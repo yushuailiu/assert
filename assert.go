57217577fe7f3599b13ab0b694c1a48f1ec1ae83
Po <admin@liuyushuai.com>
2021-05-29 16:30:37 +0800

    feat: add new assert 1. MapHasValue 2. MapNotHasKey 3. MapHasKeysOnly 4. Contains 5. NotContains 6. StringContainsString 7. StringContainsStringIgnoringCase 8. StringNotContainsString 9. StringNotContainsStringIgnoringCase

--- assert.go
+++ assert.go
@@ -5,6 +5,7 @@ import (
 	"path"
 	"reflect"
 	"runtime"
+	"strings"
 	"testing"
 	"unsafe"
 )
@@ -14,12 +15,13 @@ func Equal(t *testing.T, val1, val2 interface{}) {
 	EqualSkip(t, 2, val1, val2)
 }
 
-func EqualSkip(t *testing.T, skip int, val1, val2 interface{}) {
+func EqualSkip(t *testing.T, skip int, val1, val2 interface{}) bool {
 	if !IsEqual(val1, val2) {
 		_, file, line, _ := runtime.Caller(skip)
 		fmt.Printf("%s:%d %v does not equal %v\n", path.Base(file), line, val1, val2)
 		t.Fail()
 	}
+	return true
 }
 
 // assert val1 is not equal to val2
@@ -93,17 +95,139 @@ CASE4:
 	return reflect.DeepEqual(v1, v2)
 }
 
+// assert map has key
+func MapHasKey(t *testing.T, m interface{}, i interface{}) {
+	ValueIsNullSkip(t, m, 2)
+	m1 := interfaceToMapSkip(t, m, 2)
+	//m1 := m.(map[interface{}]interface{})
+	if !TypeIsComparableSkip(t, i, 2) {
+		return
+	}
+	_, ok := m1[i]
+	if !ok {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("%s:%d %v map has not key %v\n", path.Base(file), line, m, i)
+		t.Fail()
+	}
+}
 
-func MapHasKey(t *testing.T, m map[interface{}]interface{}, i interface{}) {
+// assert map has value
+func MapHasValue(t *testing.T, m interface{}, i interface{}) {
 	ValueIsNullSkip(t, m, 2)
 
 	if !TypeIsComparableSkip(t, i, 2) {
 		return
 	}
-	_, ok := m[i]
-	if !ok {
+
+	m1 := interfaceToMapSkip(t, m, 2)
+	mv := reflect.ValueOf(m1)
+	for _, key := range mv.MapKeys() {
+		if IsEqual(mv.MapIndex(key).Interface(), i) {
+			return
+		}
+	}
+
+	_, file, line, _ := runtime.Caller(1)
+	fmt.Printf("%s:%d %v map not has value %v \n", path.Base(file), line, m, i)
+	t.Fail()
+}
+
+// assert map not has key
+func MapNotHasKey(t *testing.T, m interface{}, i interface{}) {
+	ValueIsNullSkip(t, m, 2)
+
+	m1 := interfaceToMapSkip(t, m, 2)
+
+	if !TypeIsComparableSkip(t, i, 2) {
+		return
+	}
+	_, ok := m1[i]
+	if ok {
 		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("%s:%d %v map has not key %v", path.Base(file), line, m, i)
+		fmt.Printf("%s:%d %v map has key %v \n", path.Base(file), line, m, i)
+		t.Fail()
+	}
+}
+
+// map only has same keys
+func MapHasKeysOnly(t *testing.T, m interface{}, keys []interface{}) {
+	m1 := interfaceToMapSkip(t, m, 2)
+	allKeys := getMapKeys(m1)
+	for _, key := range allKeys {
+		if !contains(keys, key) {
+			_, file, line, _ := runtime.Caller(1)
+			fmt.Printf("%s:%d %v not only contains keys %v \n", path.Base(file), line, m, keys)
+			t.Fail()
+		}
+	}
+}
+
+// assert slice container
+func Contains(t *testing.T, l interface{}, v interface{}) {
+	s := interfaceToSliceSkip(t, l, 2)
+	if contains(s ,v) {
+		return
+	}
+	_, file, line, _ := runtime.Caller(1)
+	fmt.Printf("%s:%d %v not contains %v \n", path.Base(file), line, l, v)
+	t.Fail()
+}
+
+func contains(l []interface{}, v interface{}) bool {
+	for _, item := range l {
+		if IsEqual(v, item) {
+			return true
+		}
+	}
+	return false
+}
+
+// assert slice not contains v
+func NotContains(t *testing.T, l interface{}, v interface{}) {
+	s := interfaceToSliceSkip(t, l, 2)
+	if contains(s ,v) {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("%s:%d %v contains %v \n", path.Base(file), line, l, v)
+		t.Fail()
+	}
+}
+
+// assert string s contains substr
+func StringContainsString(t *testing.T, s, substr string) {
+	if !strings.Contains(s, substr) {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("%s:%d string '%v' not container string '%v'\n", path.Base(file), line, s, substr)
+		t.Fail()
+	}
+}
+
+// assert string s ignore case contains substr
+func StringContainsStringIgnoringCase(t *testing.T, s, substr string) {
+	s1 := strings.ToLower(s)
+	substr1 := strings.ToLower(substr)
+	if !strings.Contains(s1, substr1) {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("%s:%d string '%v' ignore case not container string '%v' \n", path.Base(file), line, s, substr)
+		t.Fail()
+	}
+}
+
+// assert string s not contains substr
+func StringNotContainsString(t *testing.T, s, substr string) {
+	if strings.Contains(s, substr) {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("%s:%d string '%v' container string '%v' \n", path.Base(file), line, s, substr)
+		t.Fail()
+	}
+}
+
+// assert string s ignore case not contains substr
+func StringNotContainsStringIgnoringCase(t *testing.T, s, substr string) {
+	s1 := strings.ToLower(s)
+	substr1 := strings.ToLower(substr)
+	if strings.Contains(s1, substr1) {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("%s:%d string '%v' ingore case container string '%v' \n", path.Base(file), line, s, substr)
 		t.Fail()
 	}
 }
@@ -117,7 +241,7 @@ func TypeIsComparableSkip(t *testing.T, i interface{}, skip int) bool {
 		return true
 	}
 	_, file, line, _ := runtime.Caller(skip)
-	fmt.Printf("%s:%d %v is not comparable", path.Base(file), line, i)
+	fmt.Printf("%s:%d %v is not comparable\n", path.Base(file), line, i)
 	t.Fail()
 	return false
 }
@@ -137,7 +261,7 @@ func ValueIsNullSkip(t *testing.T, i interface{}, skip int)  {
 	}
 	if isNull {
 		_, file, line, _ := runtime.Caller(skip)
-		fmt.Printf("%s:%d %v is not null", path.Base(file), line, i)
+		fmt.Printf("%s:%d %v is not null\n", path.Base(file), line, i)
 		t.Fail()
 	}
 }
@@ -161,4 +285,37 @@ func getMapKeys(m map[interface{}]interface{}) []interface{} {j := 0
 		j++
 	}
 	return keys
+}
+
+func interfaceToMapSkip(t *testing.T,i interface{}, skip int) map[interface{}]interface{} {
+	v := reflect.ValueOf(i)
+	if v.Kind() != reflect.Map {
+		_, file, line, _ := runtime.Caller(skip)
+		fmt.Printf("%s:%d  '%v' is not map type is %s\n", path.Base(file), line, i, v.Kind().String())
+		t.Fail()
+	}
+	m := make(map[interface{}]interface{})
+	for _, key := range v.MapKeys() {
+		m[key.Interface()] = v.MapIndex(key).Interface()
+	}
+	return m
+}
+
+func interfaceToSliceSkip(t *testing.T, i interface{}, skip int) []interface{} {
+	v := reflect.ValueOf(i)
+	if v.Kind() != reflect.Array && v.Kind() != reflect.Slice {
+		_, file, line, _ := runtime.Caller(skip)
+		fmt.Printf("%s:%d  '%v' is not slice、array type is %s\n", path.Base(file), line, i, v.Kind().String())
+		t.Fail()
+	}
+	m := make([]interface{}, v.Len())
+	for i := 0; i < v.Len(); i++ {
+		m[i] = v.Index(i).Interface()
+	}
+	return m
+}
+
+func isMap(i interface{}) bool {
+	v := reflect.ValueOf(i)
+	return v.Kind() == reflect.Map
 }
--- assert_test.go
+++ assert_test.go
@@ -1,6 +1,7 @@
 package assert
 
 import (
+	"errors"
 	"testing"
 )
 
@@ -125,5 +126,288 @@ func TestMapHasKey(t *testing.T) {
 
 	for _, items := range mapHasKeyList {
 		MapHasKey(t, items[0].(map[interface{}]interface{}), items[1])
+	}
+
+	MapHasKey(t, map[string]string{"m":"v","m1":"v1"}, "m")
+	MapHasKey(t, map[int]string{1:"v",2:"v1"}, 1)
+	MapHasKey(t, map[uint8]string{uint8(1):"v",uint8(2):"v1"}, uint8(1))
+}
+
+func TestMapHasValue(t *testing.T) {
+
+	b := make(map[interface{}]int)
+	b[&structDemo{val:1}] = 22
+	c := &structDemo{val:2}
+	f := &structDemo{val:3}
+
+	s := structDemo{val:1}
+
+	var a interface{}
+	a = 1
+
+	ch1 := make(chan int)
+
+	mapHasValueList := [][]interface{}{
+		[]interface{}{map[interface{}]interface{}{nil:"ff"}, "ff"},
+		[]interface{}{map[interface{}]interface{}{1:true}, true},
+		[]interface{}{map[interface{}]interface{}{true:1}, 1},
+		[]interface{}{map[interface{}]interface{}{"ffs":""}, ""},
+		[]interface{}{map[interface{}]interface{}{"":"ff"}, "ff"},
+		[]interface{}{map[interface{}]interface{}{ch1:ch1}, ch1},
+		[]interface{}{map[interface{}]interface{}{[1]int{1}:"ff"},"ff"},
+		[]interface{}{map[interface{}]interface{}{[1]chan int{ch1}:ch1}, ch1},
+		[]interface{}{map[interface{}]interface{}{[1]bool{true}:"ff"}, "ff"},
+		[]interface{}{map[interface{}]interface{}{[1]string{""}:"ff"}, "ff"},
+		[]interface{}{map[interface{}]interface{}{[1]*structDemo{f}:[1]*structDemo{f}},[1]*structDemo{f}},
+		[]interface{}{map[interface{}]interface{}{[1]interface{}{a}:"ff"}, "ff"},
+		[]interface{}{map[interface{}]interface{}{[1]structDemo{s}:"ff"}, "ff"},
+		[]interface{}{map[interface{}]interface{}{[1]int{1}:"ff"}, "ff"},
+		[]interface{}{map[interface{}]interface{}{"":"ff"},"ff"},
+		[]interface{}{map[interface{}]interface{}{a:"ff"},"ff"},
+		[]interface{}{map[interface{}]interface{}{a:"ff"},"ff"},
+		[]interface{}{map[interface{}]interface{}{struct {}{}:"ff"}, "ff"},
+		[]interface{}{map[interface{}]interface{}{&struct {}{}:"ff"}, "ff"}, // why ??
+		[]interface{}{map[interface{}]interface{}{1.1:1.2}, 1.2},
+		[]interface{}{map[interface{}]interface{}{uint(1):uint(1)}, uint(1)},
+		[]interface{}{map[interface{}]interface{}{uint8(1):uint8(1)}, uint8(1)},
+		[]interface{}{map[interface{}]interface{}{uint16(1):uint16(1)}, uint16(1)},
+		[]interface{}{map[interface{}]interface{}{uint32(1):uint32(1)}, uint32(1)},
+		[]interface{}{map[interface{}]interface{}{int(1):int(1)}, int(1)},
+		[]interface{}{map[interface{}]interface{}{int8(1):int8(1)},int8(1)},
+		[]interface{}{map[interface{}]interface{}{int16(1):int16(1)},int16(1)},
+		[]interface{}{map[interface{}]interface{}{int32(1):int32(1)},int32(1)},
+		[]interface{}{map[interface{}]interface{}{int64(1):int64(1)},int64(1)},
+		[]interface{}{map[interface{}]interface{}{f:f}, f},
+		[]interface{}{map[interface{}]interface{}{c:c}, c},
+	}
+
+	for _, items := range mapHasValueList {
+		MapHasValue(t, items[0].(map[interface{}]interface{}), items[1])
+	}
+
+	MapHasValue(t, map[string]string{"m":"v","m1":"v1"}, "v1")
+	MapHasValue(t, map[int]string{1:"v",2:"v1"}, "v")
+	MapHasValue(t, map[uint8]string{uint8(1):"v",uint8(2):"v1"}, "v1")
+}
+
+func TestMapNotHasKey(t *testing.T) {
+
+	b := make(map[interface{}]int)
+	b[&structDemo{val:1}] = 22
+	c := &structDemo{val:2}
+	f := &structDemo{val:3}
+
+	s := structDemo{val:1}
+
+	var a interface{}
+	a = 1
+
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	mapHasKeyList := [][]interface{}{
+		[]interface{}{map[interface{}]interface{}{nil:"ff"}, ""},
+		[]interface{}{map[interface{}]interface{}{nil:"ff"}, 0},
+		[]interface{}{map[interface{}]interface{}{nil:"ff"}, struct {}{}},
+		[]interface{}{map[interface{}]interface{}{1:"ff"},1.0},
+		[]interface{}{map[interface{}]interface{}{true:"ff"},false},
+		[]interface{}{map[interface{}]interface{}{"ffs":"ff"},"ff"},
+		[]interface{}{map[interface{}]interface{}{"":"ff"},"0"},
+		[]interface{}{map[interface{}]interface{}{ch1:"ff"},nil},
+		[]interface{}{map[interface{}]interface{}{ch1:"ff"},ch2},
+		[]interface{}{map[interface{}]interface{}{[1]int{1}:"ff"},[1]int{2}},
+		[]interface{}{map[interface{}]interface{}{[1]chan int{ch1}:"ff"},[1]chan int{ch2}},
+		[]interface{}{map[interface{}]interface{}{[1]bool{true}:"ff"},[1]bool{false}},
+		[]interface{}{map[interface{}]interface{}{[1]string{""}:"ff"},[1]interface{}{nil}},
+		[]interface{}{map[interface{}]interface{}{[1]*structDemo{f}:"ff"},[1]*structDemo{c}},
+		[]interface{}{map[interface{}]interface{}{[1]interface{}{a}:"ff"},[1]interface{}{2}},
+		[]interface{}{map[interface{}]interface{}{[1]structDemo{s}:"ff"},[1]structDemo{*f}},
+		[]interface{}{map[interface{}]interface{}{[1]int{1}:"ff"},[1]float32{1}},
+		[]interface{}{map[interface{}]interface{}{"":"ff"},nil},
+		[]interface{}{map[interface{}]interface{}{a:"ff"},uint(1)},
+		[]interface{}{map[interface{}]interface{}{a:"ff"},uint8(1)},
+		[]interface{}{map[interface{}]interface{}{a:"ff"},uint16(1)},
+		[]interface{}{map[interface{}]interface{}{a:"ff"},uint32(1)},
+		[]interface{}{map[interface{}]interface{}{a:"ff"},uint64(1)},
+		[]interface{}{map[interface{}]interface{}{a:"ff"},int64(1)},
+		[]interface{}{map[interface{}]interface{}{a:"ff"},int32(1)},
+		[]interface{}{map[interface{}]interface{}{a:"ff"},int16(1)},
+		[]interface{}{map[interface{}]interface{}{a:"ff"},int8(1)},
+		[]interface{}{map[interface{}]interface{}{a:"ff"},float32(1)},
+		[]interface{}{map[interface{}]interface{}{a:"ff"},float64(1)},
+	}
+
+	for _, items := range mapHasKeyList {
+		MapNotHasKey(t, items[0].(map[interface{}]interface{}), items[1])
+	}
+
+	dd := make(map[string]string)
+	MapNotHasKey(t, dd, "ff")
+	MapNotHasKey(t, map[string]string{"m":"v","m1":"v1"}, "mss")
+
+}
+
+func TestMapHasKeysOnly(t *testing.T) {
+
+	b := make(map[interface{}]int)
+	b[&structDemo{val:1}] = 22
+	c := &structDemo{val:2}
+	f := &structDemo{val:3}
+
+	s := structDemo{val:1}
+
+	var a interface{}
+	a = 1
+
+	ch1 := make(chan int)
+
+	mapHasKeyList := [][]interface{}{
+		[]interface{}{map[interface{}]interface{}{nil:"ff"}, []interface{}{nil}},
+		[]interface{}{map[interface{}]interface{}{1:"ff"}, []interface{}{1}},
+		[]interface{}{map[interface{}]interface{}{true:"ff"}, []interface{}{true}},
+		[]interface{}{map[interface{}]interface{}{"ffs":"ff"}, []interface{}{"ffs"}},
+		[]interface{}{map[interface{}]interface{}{"":"ff"}, []interface{}{""}},
+		[]interface{}{map[interface{}]interface{}{ch1:"ff"}, []interface{}{ch1}},
+		[]interface{}{map[interface{}]interface{}{[1]int{1}:"ff"}, []interface{}{[1]int{1}}},
+		[]interface{}{map[interface{}]interface{}{[1]chan int{ch1}:"ff"}, []interface{}{[1]chan int{ch1}}},
+		[]interface{}{map[interface{}]interface{}{[1]bool{true}:"ff"}, []interface{}{[1]bool{true}}},
+		[]interface{}{map[interface{}]interface{}{[1]string{""}:"ff"}, []interface{}{[1]string{""}}},
+		[]interface{}{map[interface{}]interface{}{[1]*structDemo{f}:"ff"}, []interface{}{[1]*structDemo{f}}},
+		[]interface{}{map[interface{}]interface{}{[1]interface{}{a}:"ff"}, []interface{}{[1]interface{}{a}}},
+		[]interface{}{map[interface{}]interface{}{[1]structDemo{s}:"ff"}, []interface{}{[1]structDemo{s}}},
+		[]interface{}{map[interface{}]interface{}{[1]int{1}:"ff"}, []interface{}{[1]int{1}}},
+		[]interface{}{map[interface{}]interface{}{"":"ff"}, []interface{}{""}},
+		[]interface{}{map[interface{}]interface{}{a:"ff"}, []interface{}{1}},
+		[]interface{}{map[interface{}]interface{}{a:"ff"}, []interface{}{a}},
+		[]interface{}{map[interface{}]interface{}{struct {}{}:"ff"}, []interface{}{struct {}{}}},
+		[]interface{}{map[interface{}]interface{}{&struct {}{}:"ff"}, []interface{}{&struct {}{}}}, // why ??
+		[]interface{}{map[interface{}]interface{}{1.0:"ff"}, []interface{}{1.0}},
+		[]interface{}{map[interface{}]interface{}{uint(1):"ff"}, []interface{}{uint(1)}},
+		[]interface{}{map[interface{}]interface{}{uint8(1):"ff"}, []interface{}{uint8(1)}},
+		[]interface{}{map[interface{}]interface{}{uint16(1):"ff"}, []interface{}{uint16(1)}},
+		[]interface{}{map[interface{}]interface{}{uint32(1):"ff"}, []interface{}{uint32(1)}},
+		[]interface{}{map[interface{}]interface{}{int(1):"ff"}, []interface{}{1}},
+		[]interface{}{map[interface{}]interface{}{int8(1):"ff"}, []interface{}{int8(1)}},
+		[]interface{}{map[interface{}]interface{}{int16(1):"ff"}, []interface{}{int16(1)}},
+		[]interface{}{map[interface{}]interface{}{int32(1):"ff"}, []interface{}{int32(1)}},
+		[]interface{}{map[interface{}]interface{}{int64(1):"ff"}, []interface{}{int64(1)}},
+		[]interface{}{map[interface{}]interface{}{f:"ff", uint8(1): "dd"}, []interface{}{f, uint8(1)}},
+		[]interface{}{map[interface{}]interface{}{c:"ff", uint(1): "ss"}, []interface{}{c, uint(1)}},
+	}
+
+	for _, items := range mapHasKeyList {
+		MapHasKeysOnly(t, items[0].(map[interface{}]interface{}), items[1].([]interface{}))
+	}
+}
+
+func TestContains(t *testing.T) {
+
+	a := &structDemo{val:1}
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	containersList := [][]interface{}{
+		{[]interface{}{1,2}, 2,},
+		{[]interface{}{1,"f"}, "f",},
+		{[]interface{}{"lj",""}, "",},
+		{[]interface{}{structDemo{val:1},1}, structDemo{val:1},},
+		{[]interface{}{structDemo{val:1}, structDemo{val:2}}, structDemo{val:2},},
+		{[]interface{}{1}, 1,},
+		{[]interface{}{true, 1}, true,},
+		{[]interface{}{nil, false}, false,},
+		{[]interface{}{nil, false}, false,},
+		{[]interface{}{a, false}, a,},
+		{[]interface{}{ch1, ch2}, ch1,},
+		{[]interface{}{ch1, ch2}, ch2,},
+		{[]interface{}{errors.New("ff"), ch2}, errors.New("ff"),},
+		{[]interface{}{errors.New("ff"), structDemo{val:1}}, structDemo{val:1},},
+	}
+
+	for _, items := range containersList {
+		Contains(t, items[0], items[1])
+	}
+
+	c := [2]int{2,3}
+	Contains(t, c, 2)
+}
+
+func TestNotContains(t *testing.T) {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	containersList := [][]interface{}{
+		{[]interface{}{1,2}, 3,},
+		{[]interface{}{1,""}, "f",},
+		{[]interface{}{"lj",0}, "",},
+		{[]interface{}{structDemo{val:2},1}, structDemo{val:1},},
+		{[]interface{}{structDemo{val:1}, structDemo{val:3}}, structDemo{val:2},},
+		{[]interface{}{2}, 1,},
+		{[]interface{}{false, 1}, true,},
+		{[]interface{}{nil, true}, false,},
+		{[]interface{}{nil, true}, false,},
+		{[]interface{}{ch2}, ch1,},
+		{[]interface{}{ch1}, ch2,},
+		{[]interface{}{errors.New("ffd"), ch2}, errors.New("ff"),},
+		{[]interface{}{errors.New("ff"), structDemo{val:2}}, structDemo{val:1},},
+	}
+
+	for _, items := range containersList {
+		NotContains(t, items[0], items[1])
+	}
+
+	c := [2]int{2,3}
+	NotContains(t, c, 4)
+}
+
+func TestStringContainsString(t *testing.T) {
+	a := [][]string{
+		{"testString", "test"},
+		{"testString", "g"},
+		{"testString", "ing"},
+		{"testString", "Strin"},
+		{"testString", "te"},
+		{"testString", "t"},
+		{"testString&", "&"},
+	}
+
+	for _, items := range a {
+		StringContainsString(t, items[0], items[1])
+	}
+}
+
+func TestStringContainsStringIgnoringCase(t *testing.T) {
+	a := [][]string{
+		{"testString", "string"},
+		{"testString", "G"},
+		{"testString", "Ing"},
+		{"testString", "strin"},
+		{"testString", "Te"},
+		{"testString", "T"},
+		{"testSTRING&", "str"},
+		{"testSTRING&", "Str"},
+	}
+
+	for _, items := range a {
+		StringContainsStringIgnoringCase(t, items[0], items[1])
+	}
+}
+
+func TestStringNotContainsString(t *testing.T) {
+	a := [][]string{
+		{"testString", "yushuailiu"},
+		{"TestString", "ss"},
+	}
+
+	for _, items := range a {
+		StringNotContainsStringIgnoringCase(t, items[0], items[1])
+	}
+}
+
+func TestStringNotContainsStringIgnoringCase(t *testing.T) {
+	a := [][]string{
+		{"testString", "STRINGsss"},
+		{"TestString", "testStrings"},
+	}
+
+	for _, items := range a {
+		StringNotContainsStringIgnoringCase(t, items[0], items[1])
 	}
 }
